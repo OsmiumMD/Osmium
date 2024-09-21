07810579b798e5588a6bf06be92acd5d2761e3a9
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-21 16:12:59 +0200

    feat: add a base for styles on vault selection
    heavily inspired from Obsidian

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -21,12 +21,38 @@ const HomeView: Component = () => {
   }
 
   return (
-    <div>
-      <p>no vault is selected</p>
-      
-      <button type="button" onClick={handleSelectVault}>
-        Select a vault
-      </button>
+    <div class="h-screen w-screen bg-[#1e1e1e] text-white select-none">
+      <div class="flex h-full">
+        <nav class="bg-[#262626] w-[260px] flex-shrink-0 border-r border-[#363636] p-6">
+          <p class="text-xs opacity-60">Here shoud come the latest vaults opened, but that feature is not developed yet...</p>
+        </nav>
+
+        <main class="flex flex-col items-center py-12 w-full">
+          <h1 class="text-2xl font-medium">
+            Osmium
+          </h1>
+          <p class="text-sm opacity-60">
+            Version 0.0.0
+          </p>
+
+          <div class="flex flex-col w-full max-w-[440px] pt-12">
+            <section class="flex justify-between">
+              <div class='flex flex-col gap-1'>
+                <h2>
+                  Open folder as vault 
+                </h2>
+                <p class="text-xs opacity-80">
+                  Choose an existing folder of Markdown files.
+                </p>
+              </div>
+              <button type="button" onClick={handleSelectVault}>
+                Open
+              </button>
+            </section>
+          </div>
+        </main>
+
+      </div>
     </div>
   )
 };
